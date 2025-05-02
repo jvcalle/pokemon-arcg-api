86cb777f388ce1633b131c9ca67defe046582205
jvcalle <137278738+jvcalle@users.noreply.github.com>
2025-05-02 20:23:21 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -4,12 +4,6 @@ define('DB_USER', $_ENV['MYSQLUSER']);
 define('DB_PASS', $_ENV['MYSQLPASSWORD']);
 define('DB_NAME', $_ENV['MYSQLDATABASE']);
 define('DB_PORT', $_ENV['MYSQLPORT']);
-
-echo DB_HOST;
-echo DB_USER;
-echo DB_PASS;
-echo DB_NAME;
-echo DB_PORT;
     
 $conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 if($conexion->connect_errno){
